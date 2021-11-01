d6ca6384caed5f90f214c44b94b9f4e7ca892e01
mltomlins0n <martin070891@gmail.com>
2021-11-01 17:12:54 +0000

    Add mock todos

---
+++ src/app/mock-todos.ts
@@ -0,0 +1,9 @@
+import { Todo } from './todo';
+
+export const TODOS: Todo[] = [
+  { name: 'this', isChecked: false },
+  { name: 'that', isChecked: false },
+  { name: 'another thing', isChecked: false },
+  { name: 'one more thing', isChecked: false },
+  { name: 'this again', isChecked: false },
+];
