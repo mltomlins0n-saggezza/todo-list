f2cd447b7ae346d2307aa3262355978d7f689c17
mltomlins0n <martin070891@gmail.com>
2021-11-02 18:13:55 +0000

    Add ids to mock todo items

--- src/app/mock-todos.ts
+++ src/app/mock-todos.ts
@@ -1,9 +1,9 @@
 import { Todo } from './todo';
 
 export const TODOS: Todo[] = [
-  { name: 'this', isChecked: false },
-  { name: 'that', isChecked: false },
-  { name: 'another thing', isChecked: false },
-  { name: 'one more thing', isChecked: false },
-  { name: 'this again', isChecked: false },
+  { id: 1, name: 'this', isChecked: false },
+  { id: 2, name: 'that', isChecked: false },
+  { id: 3, name: 'another thing', isChecked: false },
+  { id: 4, name: 'one more thing', isChecked: false },
+  { id: 5, name: 'this again', isChecked: false },
 ];
