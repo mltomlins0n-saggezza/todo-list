f4d1b05c8a9caaff7425fd2c27d951abcfdb6076
mltomlins0n <martin070891@gmail.com>
2021-11-02 14:58:52 +0000

    Add "edit-todo" component to DOM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { EditTodoComponent } from './edit-todo/edit-todo.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    EditTodoComponent
   ],
   imports: [
     BrowserModule,
