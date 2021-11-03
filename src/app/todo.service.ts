1b0f59ebc26cffe2fc65b9148b075a000b2e886f
mltomlins0n <martin070891@gmail.com>
2021-11-03 11:36:19 +0000

    Remove unused imports

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -3,7 +3,6 @@ import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Todo } from './todo';
-import { TODOS } from './mock-todos';
 
 @Injectable({
   providedIn: 'root',
