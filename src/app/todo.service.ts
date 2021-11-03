af65493d8c2e535c7af654d02573df39412bc8ef
mltomlins0n <martin070891@gmail.com>
2021-11-03 12:08:30 +0000

    Add post method, http headers, and error handling

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Todo } from './todo';
@@ -10,9 +11,30 @@ import { Todo } from './todo';
 export class TodoService {
   private todosUrl = 'api/todos';
 
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+
   constructor(private http: HttpClient) { }
 
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.todosUrl);
+    return this.http.get<Todo[]>(this.todosUrl).pipe(
+      catchError(this.handleError<Todo[]>([]))
+    );
+  }
+
+  // POST a new todo to the server
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.todosUrl, todo, this.httpOptions).pipe(
+      catchError(this.handleError<Todo>())
+    );
+  }
+
+  private handleError<T>(result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      // return an empty result so the app keeps running
+      return of(result as T);
+    }
   }
 }
