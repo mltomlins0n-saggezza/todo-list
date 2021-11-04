14ac6c742c9757dddec8d91bc35ff0e1c9043083
mltomlins0n <martin070891@gmail.com>
2021-11-04 14:32:12 +0000

    Edit component now takes in a todo object from list

--- src/app/edit-todo/edit-todo.component.ts
+++ src/app/edit-todo/edit-todo.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Todo } from '../todo';
+import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-edit-todo',
@@ -6,12 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-todo.component.css'],
 })
 export class EditTodoComponent implements OnInit {
-  constructor() {}
+  // @Input() title: string;
+
+  @Input() todo: Todo;
+
+  constructor(private todoService: TodoService) {
+    // this.title = '';
+    this.todo = {id: 0, name: 'this', isChecked: false};
+  }
 
   ngOnInit(): void {}
 
   save(): void {
     console.log('save edited todo');
+    if (this.todo) {
+      this.todoService.updateTodo(this.todo).subscribe();
+    }
   }
 
   cancel(): void {
