13c46f9d22fabd5609b24eca223708a54dfa263b
mltomlins0n <martin070891@gmail.com>
2021-11-04 14:31:19 +0000

    Add GET, PUT, and DELETE functions

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Todo } from './todo';
@@ -17,6 +17,16 @@ export class TodoService {
 
   constructor(private http: HttpClient) { }
 
+  // GET a todo item by ID
+  getTodo(id: number): Observable<Todo> {
+    const url = `${this.todosUrl}/${id}`;
+
+    return this.http.get<Todo>(url).pipe(
+      catchError(this.handleError<Todo>())
+    );
+  }
+
+  // GET all todo items
   getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(this.todosUrl).pipe(
       catchError(this.handleError<Todo[]>([]))
@@ -26,6 +36,22 @@ export class TodoService {
   // POST a new todo to the server
   addTodo(todo: Todo): Observable<Todo> {
     return this.http.post<Todo>(this.todosUrl, todo, this.httpOptions).pipe(
+      catchError(this.handleError<Todo>())
+    );
+  }
+
+  // PUT the edited todo details on the server
+  updateTodo(todo: Todo): Observable<any> {
+    return this.http.put(this.todosUrl, todo, this.httpOptions).pipe(
+      catchError(this.handleError<any>())
+    );
+  }
+
+  // DELETE a todo from the server
+  deleteTodo(id: number): Observable<Todo> {
+    const url = `${this.todosUrl}/${id}`;
+
+    return this.http.delete<Todo>(url, this.httpOptions).pipe(
       catchError(this.handleError<Todo>())
     );
   }
