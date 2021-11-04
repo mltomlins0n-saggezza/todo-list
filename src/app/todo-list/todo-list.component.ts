52ef09eee4707f5cc4d650efbe4d363e6858e540
mltomlins0n <martin070891@gmail.com>
2021-11-04 14:33:29 +0000

    Add edit and delete functions

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 
 import { Todo } from '../todo';
 import { TodoService } from '../todo.service';
@@ -13,7 +12,11 @@ export class TodoListComponent implements OnInit {
   todos: Todo[] = [];
   isVisible = false;
 
-  constructor(private todoService: TodoService) {}
+  editedTodo: Todo;
+
+  constructor(private todoService: TodoService) {
+    this.editedTodo = {id: 0, name: 'test', isChecked: false};
+  }
 
   ngOnInit(): void {
     this.getTodos();
@@ -32,13 +35,16 @@ export class TodoListComponent implements OnInit {
     })
   }
 
-  edit(): void {
+  edit(todo: Todo): void {
     console.log('Item details will be edited here');
     this.isVisible = !this.isVisible;
+    this.editedTodo = todo;
   }
 
-  delete(): void {
-    console.log('Item will be deleted here');
+  delete(todo: Todo): void {
+    this.todos = this.todos.filter(todoToDelete => todoToDelete !== todo);
+    this.todoService.deleteTodo(todo.id).subscribe();
+    this.isVisible = false; // Hide the edit pane on deletion
   }
 
   markDone(): void {
