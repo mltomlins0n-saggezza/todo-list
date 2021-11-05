8f5f120e864f07134f282ea5b2fcba1cdb04552d
mltomlins0n <martin070891@gmail.com>
2021-11-05 17:10:33 +0000

    Import Http Client Test module, mock the todo service

--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -1,25 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { TodoListComponent } from './todo-list.component';
+import { TodoService } from '../todo.service';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
+  let mockTodoService: Partial<TodoService>;
 
   beforeEach(async () => {
+    // mockTodoService = {
+    //   id: 0,
+    //   name: 'test todo item',
+    //   isChecked: false
+    // };
+
     await TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
-    })
-    .compileComponents();
+      declarations: [TodoListComponent],
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: TodoService, useValue: mockTodoService }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoListComponent);
     component = fixture.componentInstance;
+    mockTodoService = fixture.debugElement.injector.get(TodoService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
