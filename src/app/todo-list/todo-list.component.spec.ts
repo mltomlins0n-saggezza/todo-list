bfe1e9f3037c9f47afd10c42c504db621c44f040
mltomlins0n <martin070891@gmail.com>
2021-11-08 18:01:14 +0000

    Implement mock service, add tests to the component

--- src/app/todo-list/todo-list.component.spec.ts
+++ src/app/todo-list/todo-list.component.spec.ts
@@ -1,21 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { TodoListComponent } from './todo-list.component';
 import { TodoService } from '../todo.service';
+import { TEST_TODOS, TODOS } from '../mock-todos';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
-  let mockTodoService: Partial<TodoService>;
+
+  const testItem = { id: 1, name: 'TEST this', isChecked: false };
+
+  const mockTodoService = jasmine.createSpyObj<TodoService>('TodoService', {
+    getTodos: of(TEST_TODOS),
+    addTodo: of(testItem),
+    deleteTodo: of(testItem),
+  });
 
   beforeEach(async () => {
-    // mockTodoService = {
-    //   id: 0,
-    //   name: 'test todo item',
-    //   isChecked: false
-    // };
-
     await TestBed.configureTestingModule({
       declarations: [TodoListComponent],
       imports: [HttpClientTestingModule],
@@ -26,11 +29,48 @@ describe('TodoListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoListComponent);
     component = fixture.componentInstance;
-    mockTodoService = fixture.debugElement.injector.get(TodoService);
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a list with the first item being "TEST this"', () => {
+    expect(component.todos).toEqual(TEST_TODOS);
+  });
+
+  it('should NOT have a list with the first item being "this"', () => {
+    expect(component.todos).not.toEqual(TODOS);
+  });
+
+  it('should add an item to the list', () => {
+    expect(component.todos).toEqual(TEST_TODOS);
+    component.add('test add item');
+    fixture.detectChanges();
+    expect(testItem.name).toEqual('TEST this');
+  });
+
+  it('should delete an item from the list', () => {
+    expect(component.todos).toEqual(TEST_TODOS);
+    component.delete({ id: 1, name: 'TEST this', isChecked: false });
+    fixture.detectChanges();
+    expect(component.todos).not.toContain({
+      id: 1, name: 'TEST this', isChecked: false,
+    });
+  });
+
+  it('should show the edit component', () => {
+    expect(component.isVisible).toBeFalsy();
+    component.openEditView(testItem);
+    fixture.detectChanges();
+    expect(component.isVisible).toBeTruthy();
+    expect(component.editedTodo).toEqual(testItem);
+  });
+
+  it('should display the name of the todo item in the edit component', () => {
+    component.edit(testItem.name);
+    fixture.detectChanges();
+    expect(component.editedTodo.name).toEqual(testItem.name);
   });
 });
