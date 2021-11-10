b896b3e46eb3465b71aabd83052ca7dcb8665b45
mltomlins0n <martin070891@gmail.com>
2021-11-10 17:38:47 +0000

    Add test data and tests

--- src/app/todo.service.spec.ts
+++ src/app/todo.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } 
 from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { TodoService } from './todo.service';
 import { TEST_TODOS } from './mock-todos';
@@ -10,6 +11,13 @@ describe('TodoService', () => {
   let httpTestingController: HttpTestingController;
 
   const testUrl = 'api/todos';
+  const testItem = { id: 1, name: 'TEST this', isChecked: false };
+  const newTestItem = { id: 6, name: 'NEW TEST ITEM', isChecked: false };
+  const editedTestItem = { 
+    id: 1, 
+    name: 'Edited version of TEST this', 
+    isChecked: false 
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -32,32 +40,63 @@ describe('TodoService', () => {
     service.getTodos()
       .subscribe(todos => {
         expect(todos).toBeTruthy();
-        expect(todos.length).toBe(5);
+        expect(todos.length).toBe(TEST_TODOS.length);
       });
-
       const req = httpTestingController.expectOne(testUrl);
       expect(req.request.method).toEqual('GET');
       req.flush(TEST_TODOS);
   });
 
   it('should retrieve a todo item by ID', () => {
-    service.getTodo(1)
+    service.getTodo(testItem.id)
       .subscribe(todo => {
         expect(todo).toBeTruthy();
         expect(todo.id).toBe(1);
       });
-
-      const req = httpTestingController.expectOne(`${testUrl}/1`)
+      const req = httpTestingController.expectOne(`${testUrl}/${testItem.id}`)
       expect(req.request.method).toEqual('GET');
-      req.flush(TEST_TODOS[0]);
+      req.flush(testItem);
   });
 
-  // should POST a new item to the server
+  it('should POST an item to the server', () => {
+    service.addTodo(newTestItem)
+      .subscribe(todo => {
+        expect(todo).toBeTruthy();
+        expect(todo).toEqual(newTestItem);
+      });
+      const req = httpTestingController.expectOne(testUrl);
+      expect(req.request.method).toEqual('POST');
+      req.flush(newTestItem);
+  })
 
-  // should update an item's details on the server
+  it('should update an items details on the server', () => {
+    service.updateTodo(editedTestItem)
+      .subscribe(todo => {
+        expect(todo).toBeTruthy();
+        expect(todo).toEqual(editedTestItem);
+      });
+      const req = httpTestingController.expectOne(testUrl);
+      expect(req.request.method).toEqual('PUT');
+      req.flush(editedTestItem);
+  })
 
-  // should DELETE an item from the server
+  it('should DELETE an item from the server', () => {
+    service.deleteTodo(testItem.id)
+      .subscribe(todo => {
+        expect(todo).toEqual(testItem);  
+      });
+      const req = httpTestingController.expectOne(`${testUrl}/${testItem.id}`);
+      expect(req.request.method).toEqual('DELETE');
+      req.flush(testItem);
+  })
 
-  // should return an error when API call fails
-
+  it('should return an error when an API call fails', () => {
+    service.addTodo(newTestItem)
+      .subscribe(todo => {
+        expect(todo).not.toBeDefined();
+      });
+      const req = httpTestingController.expectOne(testUrl);
+      req.flush('Test error', {status: 400, statusText: 'Bad Request'});
+      // req.error(new ErrorEvent('Test error'));
+  })
 });
