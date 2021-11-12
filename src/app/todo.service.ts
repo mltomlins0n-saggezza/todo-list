ded1d358e7afd3388091335eb164ddf60480c08c
mltomlins0n <martin070891@gmail.com>
2021-11-12 17:21:32 +0000

    Add genId()

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -13,7 +13,7 @@ export class TodoService {
   private http: HttpClient;
 
   httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
   constructor(http: HttpClient) {
@@ -23,38 +23,37 @@ export class TodoService {
   // GET a todo item by ID
   getTodo(id: number): Observable<Todo> {
     const url = `${this.todosUrl}/${id}`;
-    return this.http.get<Todo>(url).pipe(
-      catchError(this.handleError<Todo>())
-    );
+    return this.http.get<Todo>(url).pipe(catchError(this.handleError<Todo>()));
   }
 
   // GET all todo items
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.todosUrl).pipe(
-      catchError(this.handleError<Todo[]>([]))
-    );
+    return this.http
+      .get<Todo[]>(this.todosUrl)
+      .pipe(catchError(this.handleError<Todo[]>([])));
   }
 
   // POST a new todo to the server
   addTodo(todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>(this.todosUrl, todo, this.httpOptions).pipe(
-      catchError(this.handleError<Todo>())
-    );
+    todo.id = this.genId(10, 10000);
+    return this.http
+      .put<Todo>(this.todosUrl, todo, this.httpOptions)
+      .pipe(catchError(this.handleError<Todo>()));
   }
 
   // PUT the edited todo details on the server
   updateTodo(todo: Todo): Observable<any> {
-    return this.http.put(this.todosUrl, todo, this.httpOptions).pipe(
-      catchError(this.handleError<any>())
-    );
+    return this.http
+      .put(this.todosUrl, todo, this.httpOptions)
+      .pipe(catchError(this.handleError<any>()));
   }
 
   // DELETE a todo from the server
   deleteTodo(id: number): Observable<Todo> {
     const url = `${this.todosUrl}/${id}`;
-    return this.http.delete<Todo>(url, this.httpOptions).pipe(
-      catchError(this.handleError<Todo>())
-    );
+    return this.http
+      .delete<Todo>(url, this.httpOptions)
+      .pipe(catchError(this.handleError<Todo>()));
   }
 
   private handleError<T>(result?: T) {
@@ -62,6 +61,15 @@ export class TodoService {
       console.error(error);
       // return an empty result so the app keeps running
       return of(result as T);
-    }
+    };
+  }
+
+  genId(min: number, max: number): number {
+    // converts number to a string in base 32
+    // and takes the numbers after the decimal point
+    // Math.random().toString(32).substr(2, 9);
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
   }
 }
